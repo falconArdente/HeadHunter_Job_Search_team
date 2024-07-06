11cbefd9f5897aa93dba4fb43b1c9c7faa52865a
AkimOxy <akimova.oki@gmail.com>
2024-07-06 23:59:03 +0300

    добавила кнопки для проверки навигации(где возможно)

--- app/src/main/java/ru/practicum/android/diploma/SearchJobFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/SearchJobFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentSearchJobBinding
 
 
@@ -24,7 +25,14 @@ class SearchJobFragment : Fragment() {
     ): View? {
         _binding = FragmentSearchJobBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.one.setOnClickListener {
+            findNavController().navigate(R.id.action_searchJobFragment_to_filterSettingsFragment)
+        }
+    }
 
 }
