5190aefa00e0134bc8a6fa5a7bad6b51d8220f93
leonid <lvravdin@edu.hse.ru>
2024-07-08 00:13:27 +0300

    Настроены Build-gradle и Android-manifest файлы. Добавлены библиотеки: Room, Retrofit, Koin, Coroutines, Glise, Gson, Json и другие. Добавлено разрешение на интернет. Исправлены замечания detekt'а

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.practicum.android.diploma"
-        minSdk = libs.versions.targetSdk.get().toInt()
+        minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
