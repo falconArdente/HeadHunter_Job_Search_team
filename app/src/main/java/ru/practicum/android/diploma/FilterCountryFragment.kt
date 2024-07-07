21e5ec4241de2a57906b2e323bf8099358c41bea
AkimOxy <akimova.oki@gmail.com>
2024-07-07 16:04:15 +0300

    Added .idea to .gitignore

--- app/src/main/java/ru/practicum/android/diploma/FilterCountryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/FilterCountryFragment.kt
@@ -2,6 +2,6 @@ package ru.practicum.android.diploma
 
 import androidx.fragment.app.Fragment
 
-class FltrCountryFragment : Fragment() {
+class FilterCountryFragment : Fragment() {
     // лэйаут берем fragment_filter_place_to_work
 }
