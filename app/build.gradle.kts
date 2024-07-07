680bae86147422ec4ee38cf1b2c46b4e331238e8
leonid <lvravdin@edu.hse.ru>
2024-07-07 15:19:51 +0300

    Настроены Build-gradle и Android-manifest файлы. Добавлены библиотеки: Room, Retrofit, Koin, Coroutines, Glise, Gson, Json и другие. Добавлено разрешение на интернет.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+
+    // Serialization plugin
+    kotlin("plugin.serialization") version "1.9.22"
+    id("kotlin-kapt")
 }
 
 android {
@@ -10,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.practicum.android.diploma"
-        minSdk = libs.versions.minSdk.get().toInt()
+        minSdk = 26
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
@@ -36,9 +40,17 @@ android {
 
     buildFeatures {
         buildConfig = true
+        // Binding
+        viewBinding = true
     }
 }
 
+val retrofitVersion = "2.9.0"
+val koinVersion = "3.2.2"
+val coroutineVersion = "1.7.1"
+val glideVersion = "4.14.2"
+val navigationVersion = "2.7.7"
+val roomVersion = "2.6.1"
 dependencies {
     implementation(libs.androidX.core)
     implementation(libs.androidX.appCompat)
@@ -55,4 +67,40 @@ dependencies {
     androidTestImplementation(libs.uiTests.junitExt)
     androidTestImplementation(libs.uiTests.espressoCore)
     // endregion
+
+    // Constraint layout
+    implementation (libs.ui.constraintLayout)
+
+    // Fragments
+    implementation ("androidx.fragment:fragment-ktx:1.8.1")
+
+    // Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:$glideVersion")
+    annotationProcessor ("com.github.bumptech.glide:compiler:$glideVersion")
+
+    // Gson and Json (Serialization)
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Koin (DI)
+    implementation ("io.insert-koin:koin-android:$koinVersion")
+    implementation ("io.insert-koin:koin-core:$koinVersion")
+
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
+
+    // Room
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    implementation ("androidx.room:room-ktx:$roomVersion")
+    kapt ("androidx.room:room-compiler:$roomVersion")
 }
