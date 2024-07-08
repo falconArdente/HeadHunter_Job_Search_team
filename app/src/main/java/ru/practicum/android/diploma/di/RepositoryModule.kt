34cdf89ead42954676bc1a49942a7969e2325c11
Spyker3D <spyker3@yandex.ru>
2024-07-08 21:52:41 +0300

    Добавлены зависимости в DataModule для SharedPrefsStorage и Navigator

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,7 +1,18 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.details.data.repository.NavigatorRepositoryImpl
+import ru.practicum.android.diploma.details.domain.api.NavigatorRepository
+import ru.practicum.android.diploma.filter.data.repository.FilterStorageRepositoryImpl
+import ru.practicum.android.diploma.filter.domain.api.FilterStorageRepository
 
 val repositoryModule = module {
+    single<NavigatorRepository> {
+        NavigatorRepositoryImpl(externalNavigator = get())
+    }
+
+    single<FilterStorageRepository> {
+        FilterStorageRepositoryImpl(filterStorage = get())
+    }
 
 }
