ccd8e6904c146483e45d208dc7478a602bd63f72
Zimik <Zimik24@yandex.ru>
2024-07-09 23:38:20 +0500

    Создание БД, версия 2

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.di
 
 import android.content.Context
+import androidx.room.Room
 import com.google.gson.Gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.db.data.db.AppDatabase
 import ru.practicum.android.diploma.details.data.externalnavigator.ExternalNavigator
 import ru.practicum.android.diploma.filter.data.storage.FilterStorage
 import ru.practicum.android.diploma.filter.data.storage.SharedPrefsStorage
@@ -23,6 +25,15 @@ val dataModule = module {
 
     single<FilterStorage> {
         SharedPrefsStorage(sharedPreferences = get(), gson = get())
+    }
+
+    single {
+        Room.databaseBuilder(
+            androidContext(), AppDatabase::class.java, "database.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
     }
 
 }
