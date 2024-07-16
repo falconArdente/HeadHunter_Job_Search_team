69626586868f5e979b6dfc62965e283db434b814
AkimOxy <akimova.oki@gmail.com>
2024-07-16 18:15:40 +0300

    Работа поиска(без пагинации)1

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,35 +1,20 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
+import ru.practicum.android.diploma.details.presentation.viewmodel.VacancyDetailsViewModel
 import ru.practicum.android.diploma.search.presentation.viewmodel.SearchViewModel
 
 val viewModelModule = module {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    viewModel<VacancyDetailsViewModel> { (vacancyId: String) ->
+        VacancyDetailsViewModel(
+            application = androidApplication(),
+            getVacancyDetailsUseCase = get(),
+            navigatorInteractor = get(),
+            vacancyId = get { parametersOf(vacancyId) }
+        )
+    }
     viewModel<SearchViewModel> { SearchViewModel(interactor = get()) }
 }
-
