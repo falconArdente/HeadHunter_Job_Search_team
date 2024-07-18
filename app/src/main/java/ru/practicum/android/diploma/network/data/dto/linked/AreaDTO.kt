e2093ce7f2fc76bb37fcbdaadb59f87696d99c76
falconArdente <iwsokolov@ya.ru>
2024-07-19 00:57:38 +0300

    luckyB..d

--- app/src/main/java/ru/practicum/android/diploma/network/data/dto/linked/AreaDTO.kt
+++ app/src/main/java/ru/practicum/android/diploma/network/data/dto/linked/AreaDTO.kt
@@ -6,6 +6,6 @@ class AreaDTO(
     @SerializedName("areas") val subAreaDTOS: List<AreaDTO>,
     @SerializedName("id") val id: String,
     @SerializedName("name") val name: String,
-    @SerializedName("name_prepositional") val prepositional: String?,// поддерживается только для русской локализации
+    @SerializedName("name_prepositional") val prepositional: String?, // поддерживается только для русской локализации
     @SerializedName("parent_id") val parentId: String?,
 )
