1205acaa23cd3e008d89837b72df0217e16ef6aa
leonid <lvravdin@edu.hse.ru>
2024-07-20 23:33:28 +0300

    detekt durak 2

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/viewmodel/FilterSettingsViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/viewmodel/FilterSettingsViewModel.kt
@@ -6,27 +6,29 @@ import androidx.lifecycle.ViewModel
 import org.koin.core.component.KoinComponent
 import ru.practicum.android.diploma.filter.presentation.state.FilterSettingsState
 
-class FilterSettingsViewModel: ViewModel(), KoinComponent {
+class FilterSettingsViewModel : ViewModel(), KoinComponent {
 
     private val state = MutableLiveData<FilterSettingsState>()
 
     fun getState(): LiveData<FilterSettingsState> = state
-    init{
+
+    init {
         state.value = FilterSettingsState("", "", "", false)
     }
-    fun changeWorkPlace(newWorkPlace: String){
+
+    fun changeWorkPlace(newWorkPlace: String) {
         state.value = state.value?.copy(workPlace = newWorkPlace)
     }
 
-    fun changeIndustry(newIndustry: String){
+    fun changeIndustry(newIndustry: String) {
         state.value = state.value?.copy(industry = newIndustry)
     }
 
-    fun changeSalary(newSalary: String){
+    fun changeSalary(newSalary: String) {
         state.value = state.value?.copy(salary = newSalary)
     }
 
-    fun changeDontShowWithoutSalary(newDontShow: Boolean){
+    fun changeDontShowWithoutSalary(newDontShow: Boolean) {
         state.value = state.value?.copy(dontShowWithoutSalary = newDontShow)
     }
 }
