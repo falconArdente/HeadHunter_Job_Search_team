e7a53bd8726bfe37fb141b7358dd0f70da9a6b2e
leonid <lvravdin@edu.hse.ru>
2024-07-22 19:32:44 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterPlaceToWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterPlaceToWorkFragment.kt
@@ -6,17 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterSettingsBinding
-import ru.practicum.android.diploma.databinding.FragmentFilterWithRecyclerBinding
-import ru.practicum.android.diploma.filter.domain.model.Country
 
 class FilterPlaceToWorkFragment : Fragment() {
     private var _binding: FragmentFilterSettingsBinding? = null
     private val binding get() = _binding!!
-
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentFilterSettingsBinding.inflate(inflater, container, false)
         return binding.root
@@ -57,6 +52,5 @@ class FilterPlaceToWorkFragment : Fragment() {
         binding.filterFirstFilterName.text = requireActivity().getString(R.string.country)
         binding.filterSecondFilterName.text = requireActivity().getString(R.string.region)
     }
-
 
 }
