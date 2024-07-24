284f953c61e224a4e8182d582fcb2e818abab80b
Zimik <Zimik24@yandex.ru>
2024-07-24 09:41:04 +0500

    Исправлены названия таблиц(форматирование)

--- app/src/main/java/ru/practicum/android/diploma/db/data/db/entity/EmployerEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/db/data/db/entity/EmployerEntity.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.db.data.db.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Employer")
+@Entity(tableName = "employer")
 data class EmployerEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
