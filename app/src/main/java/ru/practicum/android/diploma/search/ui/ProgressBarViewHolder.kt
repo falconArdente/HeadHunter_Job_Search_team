19d6389deca33147a622a31666a9a791b24cdb7d
Spyker3D <spyker3@yandex.ru>
2024-07-26 02:44:07 +0300

    Устранены замечания detekt

--- app/src/main/java/ru/practicum/android/diploma/search/ui/ProgressBarViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/ProgressBarViewHolder.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.search.ui
 
-import android.util.Log
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.ProgressBarRecyclerItemBinding
@@ -8,7 +7,6 @@ import ru.practicum.android.diploma.databinding.ProgressBarRecyclerItemBinding
 class ProgressBarViewHolder(private val binding: ProgressBarRecyclerItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(isLastPage: Boolean) {
-        Log.d("PROGRESS_HOLDER", "$isLastPage")
         binding.loadingProgressBar.isVisible = !isLastPage
     }
 }
